3a5e375395aa8e94e184840cdbf20c46e655173c
Giorgos Nikolaou <giorgosnl17@gmail.com>
2024-12-06 12:59:13 +0200

    Sonar properties, test updates

--- tests/test_video_compressor.py
+++ tests/test_video_compressor.py
@@ -269,3 +269,94 @@ def test_compress_videos_in_directory(mock_os_walk, mock_ffmpeg, mock_logger):
 
     # Assert
     VideoCompressor.compress_video.assert_called()
+
+@patch('subprocess.run')
+def test_compress_video_cpu(mock_subprocess_run, mock_logger):
+    # Arrange
+    input_file = "path/to/input.mp4"
+    output_file = "path/to/output.mp4"
+    bitrate = "1000K" 
+    framerate = 30
+    mock_subprocess_run.return_value = mock.Mock(returncode=0)
+
+    # Act
+    VideoCompressor.compress_video_cpu(input_file, output_file, bitrate, framerate)
+
+    # Assert
+    mock_subprocess_run.assert_called_once()
+    assert mock_subprocess_run.call_args[0][0] == [
+        'ffmpeg',
+        '-i', input_file,
+        '-b:v', bitrate,
+        '-vcodec', 'libx264',
+        '-r', str(framerate), 
+        '-metadata', 'comment=compressed',
+        '-loglevel', 'error',
+        output_file
+    ]
+    mock_logger.info.assert_called_once_with(
+        f"Compressed video: {input_file} to {output_file}"
+    )
+
+def test_compress_video_cpu_error(mock_logger):
+    # Arrange
+    input_file = "path/to/input.mp4"
+    output_file = "path/to/output.mp4"
+    bitrate = "1000K"
+    framerate = 30
+    error_message = "Subprocess error"
+    mock_error = subprocess.CalledProcessError(
+        1, "cmd", stderr=error_message.encode()
+    )
+
+    with mock.patch("subprocess.run", side_effect=mock_error):
+        # Act
+        VideoCompressor.compress_video_cpu(input_file, output_file, bitrate, framerate)
+
+        # Assert
+        mock_logger.error.assert_called_once_with(
+            f"An error occurred while encoding: {input_file}. ERROR MESSAGE: {error_message}"
+        )
+
+@patch('subprocess.run')
+def test_convert_incompatible_video(mock_subprocess_run, mock_logger):
+    # Arrange
+    input_file = "path/to/input.mkv"
+    output_file = "path/to/output.mp4"
+    mock_subprocess_run.return_value = mock.Mock(returncode=0)
+
+    # Act
+    VideoCompressor.convert_incompatible_video(input_file, output_file)
+
+    # Assert
+    mock_subprocess_run.assert_called_once()
+    assert mock_subprocess_run.call_args[0][0] == [
+        'ffmpeg',
+        '-i', input_file,
+        '-c:v', 'libx264',
+        '-crf', '23',
+        '-metadata', 'comment=compressed',
+        '-preset', 'medium', 
+        output_file
+    ]
+    mock_logger.info.assert_called_once_with(
+        f"Converted video: {input_file} to {output_file}"
+    )
+
+def test_convert_incompatible_video_error(mock_logger):
+    # Arrange 
+    input_file = "path/to/input.mkv"
+    output_file = "path/to/output.mp4"
+    error_message = "Subprocess error"
+    mock_error = subprocess.CalledProcessError(
+        1, "cmd", stderr=error_message.encode()
+    )
+
+    with mock.patch("subprocess.run", side_effect=mock_error):
+        # Act
+        VideoCompressor.convert_incompatible_video(input_file, output_file)
+
+        # Assert
+        mock_logger.error.assert_called_once_with(
+            f"An error occurred while converting: {input_file}. ERROR MESSAGE: {error_message}"
+        )
