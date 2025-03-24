fcff5c43990dcbb24df8462fb4909ebe722ea5a3
Giorgos Nikolaou <giorgosnl17@gmail.com>
2025-03-24 19:22:16 +0200

    Add size calculation (#3)

--- utils/handler/handler.py
+++ utils/handler/handler.py
@@ -10,8 +10,25 @@ class Handler:
     LOGGER = None
 
     @classmethod
+    def get_directory_size(cls, directory):
+        """Calculate total size of a directory in bytes"""
+        total_size = 0
+        if os.path.isfile(directory):
+            return os.path.getsize(directory)
+        
+        for dirpath, _, filenames in os.walk(directory):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                if not os.path.islink(fp):
+                    total_size += os.path.getsize(fp)
+        return total_size
+
+    @classmethod
     def start_compression(cls, input_directory, process_video, process_image, convert_incompatible, progress_callback=None):
         timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        
+        # Get initial size before compression
+        original_size = cls.get_directory_size(input_directory)
         
         if os.path.isfile(input_directory):
             output_directory = f"{os.path.dirname(input_directory)}/output_{timestamp}"
@@ -31,6 +48,9 @@ class Handler:
                 ImageCompressor.compress_images_in_directory(input_directory, output_directory, progress_callback)
             if convert_incompatible:
                 VideoCompressor.convert_incompatible_videos_in_directory_and_compress(input_directory, output_directory, progress_callback)
+                
+            compressed_size = cls.get_directory_size(output_directory)
+            return original_size, compressed_size,
         finally:
             cls.cleanup_logging()
 
